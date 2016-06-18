5b65fc662317463246f447816a901d32be0ea8da
weiweikong <kongww.nudt@gmail.com>
2016-06-18 21:50:36 +0800

    first

---
+++ build/CMakeFiles/create_marker.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/amax/Workspace/12.opencv_contrib/aruco_standalone/samples/create_marker.cpp" "/home/amax/Workspace/12.opencv_contrib/aruco_standalone/build/CMakeFiles/create_marker.dir/samples/create_marker.cpp.o"
+  "/home/amax/Workspace/12.opencv_contrib/aruco_standalone/src/aruco.cpp" "/home/amax/Workspace/12.opencv_contrib/aruco_standalone/build/CMakeFiles/create_marker.dir/src/aruco.cpp.o"
+  "/home/amax/Workspace/12.opencv_contrib/aruco_standalone/src/dictionary.cpp" "/home/amax/Workspace/12.opencv_contrib/aruco_standalone/build/CMakeFiles/create_marker.dir/src/dictionary.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "/home/amax/Workspace/12.opencv_contrib/opencv/opencv3.0/include/opencv"
+  "/home/amax/Workspace/12.opencv_contrib/opencv/opencv3.0/include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/CMakeFiles/create_marker.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/create_marker.dir/src/aruco.cpp.o"
+  "CMakeFiles/create_marker.dir/src/dictionary.cpp.o"
+  "CMakeFiles/create_marker.dir/samples/create_marker.cpp.o"
+  "create_marker.pdb"
+  "create_marker"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/create_marker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
